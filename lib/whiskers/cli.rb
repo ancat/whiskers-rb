0f56468c0d1766f37fb4c919cf04d293b50e01ac
OMAR <946975+ancat@users.noreply.github.com>
2025-01-20 16:32:52 -0500

    some renaming

--- lib/whiskers/cli.rb
+++ lib/whiskers/cli.rb
@@ -1,4 +1,5 @@
 require 'optparse'
+require 'json'
 
 module Whiskers
   class CLI
@@ -8,32 +9,101 @@ module Whiskers
 
     def initialize(args)
       @args = args
+      @command = @args.shift
     end
 
     def run
-      options = parse_options
+      case @command
+      when "gem_diff"
+        run_gem_diff
+      when "lockfile_diff"
+        run_lockfile_diff
+      when "-h", "--help"
+        show_help
+      when "-v", "--version"
+        show_version
+      else
+        show_help
+        exit 1
+      end
+    end
+
+    private
+
+    def show_help
+      puts "Usage: whiskers COMMAND [options]"
+      puts
+      puts "Commands:"
+      puts "  gem_diff      Compare two versions of a gem"
+      puts "  lockfile_diff Compare two Gemfile.lock files"
+      puts
+      puts "Options:"
+      puts "  -h, --help     Show this help message"
+      puts "  -v, --version  Show version"
+      puts
+      puts "See 'whiskers COMMAND --help' for more information on a specific command"
+    end
+
+    def show_version
+      puts Whiskers::VERSION
+    end
+
+    def run_gem_diff
+      options = parse_gem_diff_options
       differences = compare_versions(options)
       display_differences(differences)
       scan_changes(differences, options) unless differences.values.all?(&:empty?)
     end
 
-    private
+    def run_lockfile_diff
+      options = parse_lockfile_diff_options
+      diff = GemfileLockDiff.new(options[:old_lockfile], options[:new_lockfile])
 
-    def parse_options
+      if options[:json]
+        output = {
+          added: diff.new_dependencies.sort_by(&:name).map { |dep|
+            { name: dep.name, version: dep.version }
+          },
+          changed: diff.changed_dependencies.sort_by { |c| c.from.name }.map { |change|
+            {
+              name: change.from.name,
+              old_version: change.from.version,
+              new_version: change.to.version
+            }
+          }
+        }
+        puts JSON.pretty_generate(output)
+      else
+        if diff.new_dependencies.any?
+          puts "\nAdded gems:"
+          diff.new_dependencies.sort_by(&:name).each do |dep|
+            puts "  + #{dep.name} (#{dep.version})"
+          end
+        end
+
+        if diff.changed_dependencies.any?
+          puts "\nChanged versions:"
+          diff.changed_dependencies.sort_by { |c| c.from.name }.each do |change|
+            puts "  ~ #{change.from.name}: #{change.from.version} → #{change.to.version}"
+          end
+        end
+
+        if diff.new_dependencies.empty? && diff.changed_dependencies.empty?
+          puts "No differences found between Gemfile.lock files"
+        end
+      end
+    end
+
+    def parse_gem_diff_options
       options = {}
       parser = OptionParser.new do |opts|
-        opts.banner = "Usage: whiskers [options]"
+        opts.banner = "Usage: whiskers gem_diff [options]"
 
         opts.separator ""
         opts.separator "Options:"
 
         opts.on("-h", "--help", "Show this help message") do
           puts opts
-          exit
-        end
-
-        opts.on("-v", "--version", "Show version") do
-          puts Whiskers::VERSION
           exit
         end
 
@@ -62,6 +132,57 @@ module Whiskers
         puts "Error: --gem, --old, and --new are all required"
         puts parser
         exit 1
+      end
+
+      options
+    end
+
+    def parse_lockfile_diff_options
+      options = {}
+      parser = OptionParser.new do |opts|
+        opts.banner = "Usage: whiskers lockfile_diff [options]"
+
+        opts.separator ""
+        opts.separator "Options:"
+
+        opts.on("-h", "--help", "Show this help message") do
+          puts opts
+          exit
+        end
+
+        opts.on("--json", "Output in JSON format") do
+          options[:json] = true
+        end
+
+        opts.on("--old LOCKFILE", "Path to old Gemfile.lock") do |path|
+          options[:old_lockfile] = path
+        end
+
+        opts.on("--new LOCKFILE", "Path to new Gemfile.lock") do |path|
+          options[:new_lockfile] = path
+        end
+      end
+
+      begin
+        parser.parse!(@args)
+      rescue OptionParser::InvalidOption => e
+        puts "Error: #{e.message}"
+        puts parser
+        exit 1
+      end
+
+      unless options[:old_lockfile] && options[:new_lockfile]
+        puts "Error: Both --old and --new lockfiles are required"
+        puts parser
+        exit 1
+      end
+
+      # Validate files exist
+      [options[:old_lockfile], options[:new_lockfile]].each do |file|
+        unless File.exist?(file)
+          puts "Error: File '#{file}' not found"
+          exit 1
+        end
       end
 
       options
@@ -106,7 +227,7 @@ module Whiskers
     def scan_changes(differences, options)
       puts "\nRunning Semgrep security scan on changed files..."
       comparer = GemVersionComparer.new(options[:gem_name], options[:old_version], options[:new_version])
-      
+
       # Get all files to scan in both versions
       old_files = differences[:modified].map { |f| File.join(comparer.base_dir(options[:old_version]), f) }
       new_files = differences[:modified].map { |f| File.join(comparer.base_dir(options[:new_version]), f) } +
